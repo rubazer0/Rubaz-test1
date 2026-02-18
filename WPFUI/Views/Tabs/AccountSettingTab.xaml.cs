43673733e1fcb781287057a2af436b975398961a
rubazer0 <andre.gabriel.klaus@gmail.com>
2026-02-18 19:17:37 -0300

    Update AccountSettingTab.xaml.cs

--- WPFUI/Views/Tabs/AccountSettingTab.xaml.cs
+++ WPFUI/Views/Tabs/AccountSettingTab.xaml.cs
@@ -1,4 +1,4 @@
-﻿using MainCore.UI.ViewModels.Tabs;
+using MainCore.UI.ViewModels.Tabs;
 using ReactiveUI;
 using System.Reactive.Disposables.Fluent;
 
@@ -18,10 +18,18 @@ namespace WPFUI.Views.Tabs
             InitializeComponent();
             this.WhenActivated(d =>
             {
+                // Comandos
                 this.BindCommand(ViewModel, vm => vm.ExportCommand, v => v.ExportButton).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.ImportCommand, v => v.ImportButton).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.SaveCommand, v => v.SaveButton).DisposeWith(d);
+                this.BindCommand(ViewModel, vm => vm.TestTelegramCommand, v => v.TestTelegramButton).DisposeWith(d);
 
+                // --- LIGAÇÃO DOS CAMPOS DE TEXTO DO TELEGRAM ---
+                this.Bind(ViewModel, vm => vm.TelegramToken, v => v.TelegramTokenText.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.TelegramChatId, v => v.TelegramChatIdText.Text).DisposeWith(d);
+                // -----------------------------------------------
+
+                // Outros Bindings
                 this.Bind(ViewModel, vm => vm.AccountSettingInput.ClickDelay, v => v.ClickDelay.ViewModel).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccountSettingInput.TaskDelay, v => v.TaskDelay.ViewModel).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccountSettingInput.WorkTime, v => v.WorkTime.ViewModel).DisposeWith(d);
