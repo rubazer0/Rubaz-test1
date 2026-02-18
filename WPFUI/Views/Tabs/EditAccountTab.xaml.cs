d5b8c6dfb5a814598bfe2f738c682e77f14443ed
rubazer0 <andre.gabriel.klaus@gmail.com>
2026-02-18 20:34:55 -0300

    Update EditAccountTab.xaml.cs

--- WPFUI/Views/Tabs/EditAccountTab.xaml.cs
+++ WPFUI/Views/Tabs/EditAccountTab.xaml.cs
@@ -1,9 +1,10 @@
-﻿using MainCore.UI.ViewModels.Tabs;
+using MainCore.UI.ViewModels.Tabs;
 using ReactiveUI;
 using System.Reactive.Disposables.Fluent;
 
 namespace WPFUI.Views.Tabs
 {
+    // ESTA CLASSE É FUNDAMENTAL PARA O XAML FUNCIONAR
     public class EditAccountTabBase : ReactiveUserControl<EditAccountViewModel>
     {
     }
@@ -19,16 +20,26 @@ namespace WPFUI.Views.Tabs
 
             this.WhenActivated(d =>
             {
+                // Botões de Acesso
                 this.BindCommand(ViewModel, vm => vm.AddAccessCommand, v => v.AddAccessButton).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.EditAccessCommand, v => v.EditAccessButton).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.DeleteAccessCommand, v => v.DeleteAccessButton).DisposeWith(d);
+
+                // Botão de Salvar Conta
                 this.BindCommand(ViewModel, vm => vm.EditAccountCommand, v => v.EditAccountButton).DisposeWith(d);
 
+                // Botão de Testar Telegram
+                this.BindCommand(ViewModel, vm => vm.TestTelegramCommand, v => v.TestTelegramButton).DisposeWith(d);
+
+                // Campos da Conta
                 this.Bind(ViewModel, vm => vm.AccountInput.Username, v => v.NicknameTextBox.Text).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccountInput.Server, v => v.ServerTextBox.Text).DisposeWith(d);
+
+                // Grid de Proxies
                 this.OneWayBind(ViewModel, vm => vm.AccountInput.Accesses, v => v.ProxiesDataGrid.ItemsSource).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.SelectedAccess, v => v.ProxiesDataGrid.SelectedItem).DisposeWith(d);
 
+                // Campos do Proxy (AccessInput)
                 this.Bind(ViewModel, vm => vm.AccessInput.Username, v => v.UsernameTextBox.Text).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccessInput.Password, v => v.PasswordTextBox.Text).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccessInput.ProxyHost, v => v.ProxyHostTextBox.Text).DisposeWith(d);
@@ -36,6 +47,10 @@ namespace WPFUI.Views.Tabs
                 this.Bind(ViewModel, vm => vm.AccessInput.ProxyUsername, v => v.ProxyUsernameTextBox.Text).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccessInput.ProxyPassword, v => v.ProxyPasswordTextBox.Text).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.AccessInput.Useragent, v => v.UseragentTextBox.Text).DisposeWith(d);
+
+                // Campos do Telegram
+                this.Bind(ViewModel, vm => vm.TelegramToken, v => v.TelegramTokenText.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.TelegramChatId, v => v.TelegramChatIdText.Text).DisposeWith(d);
             });
         }
     }
