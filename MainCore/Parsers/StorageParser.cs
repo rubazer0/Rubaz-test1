c932785f36199b7a715d30e612dc48bf1fc7a669
rubazer0 <andre.gabriel.klaus@gmail.com>
2026-02-18 19:20:39 -0300

    Update StorageParser.cs

--- MainCore/Parsers/StorageParser.cs
+++ MainCore/Parsers/StorageParser.cs
@@ -1,52 +1,80 @@
-﻿using System.Net;
+using HtmlAgilityPack;
+using System.Linq;
 
 namespace MainCore.Parsers
 {
     public static class StorageParser
     {
-        private static long GetResource(HtmlDocument doc, string id)
+        // Função auxiliar para limpar números do Travian (remove virgulas e caracteres invisiveis)
+        private static long ParseTravianNumber(string text)
         {
-            var node = doc.GetElementbyId(id);
-            if (node is null) return -1;
-            return node.InnerText.ParseLong();
+            if (string.IsNullOrEmpty(text)) return 0;
+            var cleanText = new string(text.Where(char.IsDigit).ToArray());
+            if (long.TryParse(cleanText, out long result)) return result;
+            return 0;
         }
 
-        public static long GetWood(HtmlDocument doc) => GetResource(doc, "l1");
-
-        public static long GetClay(HtmlDocument doc) => GetResource(doc, "l2");
-
-        public static long GetIron(HtmlDocument doc) => GetResource(doc, "l3");
-
-        public static long GetCrop(HtmlDocument doc) => GetResource(doc, "l4");
-
-        public static long GetFreeCrop(HtmlDocument doc) => GetResource(doc, "stockBarFreeCrop");
-
-        public static long GetWarehouseCapacity(HtmlDocument doc)
+        public static long GetCrop(HtmlDocument doc)
         {
-            var stockBarNode = doc.GetElementbyId("stockBar");
-            if (stockBarNode is null) return -1;
-            var warehouseNode = stockBarNode.Descendants("div").FirstOrDefault(x => x.HasClass("warehouse"));
-            if (warehouseNode is null) return -1;
-            var capacityNode = warehouseNode.Descendants("div").FirstOrDefault(x => x.HasClass("capacity"));
-            if (capacityNode is null) return -1;
-            var valueNode = capacityNode.Descendants("div").FirstOrDefault(x => x.HasClass("value"));
-            if (valueNode is null) return -1;
-            return valueNode.InnerText.ParseLong();
+            // Busca o elemento do cereal (id="l4")
+            var node = doc.GetElementbyId("l4");
+            if (node == null) return -1;
+            return ParseTravianNumber(node.InnerText);
         }
 
         public static long GetGranaryCapacity(HtmlDocument doc)
         {
-            var stockBarNode = doc.GetElementbyId("stockBar");
-            if (stockBarNode is null) return -1;
-            var granaryNode = stockBarNode.Descendants("div").FirstOrDefault(x => x.HasClass("granary"));
-            if (granaryNode is null) return -1;
-            var capacityNode = granaryNode.Descendants("div").FirstOrDefault(x => x.HasClass("capacity"));
-            if (capacityNode is null) return -1;
-            var valueNode = capacityNode.Descendants("div").FirstOrDefault(x => x.HasClass("value"));
-            if (valueNode is null) return -1;
-            var valueStrFixed = WebUtility.HtmlDecode(valueNode.InnerText);
-            if (string.IsNullOrEmpty(valueStrFixed)) return -1;
-            return valueNode.InnerText.ParseLong();
+            // 1. Procura a div principal "granary" em qualquer lugar do documento
+            var granaryNode = doc.DocumentNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("granary"));
+
+            if (granaryNode == null) return -1;
+
+            // 2. Procura a div "capacity" dentro dela
+            var capacityNode = granaryNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("capacity"));
+
+            if (capacityNode == null) return -1;
+
+            // 3. Procura a div "value" dentro de capacity
+            var valueNode = capacityNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("value"));
+
+            if (valueNode == null) return -1;
+
+            return ParseTravianNumber(valueNode.InnerText);
+        }
+
+        // Mantemos os outros métodos para compatibilidade, mas usando nossa limpeza de numero
+        public static long GetWood(HtmlDocument doc) => GetResource(doc, "l1");
+        public static long GetClay(HtmlDocument doc) => GetResource(doc, "l2");
+        public static long GetIron(HtmlDocument doc) => GetResource(doc, "l3");
+        public static long GetFreeCrop(HtmlDocument doc) => GetResource(doc, "stockBarFreeCrop");
+
+        private static long GetResource(HtmlDocument doc, string id)
+        {
+            var node = doc.GetElementbyId(id);
+            if (node is null) return -1;
+            return ParseTravianNumber(node.InnerText);
+        }
+
+        public static long GetWarehouseCapacity(HtmlDocument doc)
+        {
+            // Mesma lógica robusta para o Armazém
+            var warehouseNode = doc.DocumentNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("warehouse"));
+
+            if (warehouseNode == null) return -1;
+
+            var capacityNode = warehouseNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("capacity"));
+            if (capacityNode == null) return -1;
+
+            var valueNode = capacityNode.Descendants("div")
+                .FirstOrDefault(x => x.HasClass("value"));
+            if (valueNode == null) return -1;
+
+            return ParseTravianNumber(valueNode.InnerText);
         }
     }
 }
